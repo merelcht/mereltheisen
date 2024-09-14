ed01588308c2d75d856b62cdadee4b381897a58a
MerelCHT <mereltheisen@gmail.com>
2024-09-14 17:08:49 +0100

    Update domain settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from "./src/utils/all";
 
 export default defineConfig({
-  site: 'https://mereltheisen.com',
+  site: 'http://mereltheisen.com',
   markdown: {
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: ["rehype-plugin-image-native-lazy-loading"],
