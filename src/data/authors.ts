8c7dbf5f6c2301ebf1e400813eac052cc08717ae
MerelCHT <mereltheisen@gmail.com>
2026-01-02 14:03:20 +0100

    Update how images are handled

--- src/data/authors.ts
+++ src/data/authors.ts
@@ -1,17 +1,19 @@
+import type { StaticImageData } from "astro:assets";
 export interface Props {
   name: string;
   slug: string;
-  image: string;
+  image: StaticImageData;
   bio: string;
 }
 
 export type Author = Props;
 
+import aboutMe from "../assets/authors/about-me.png";
 export const authors: Props[] = [
   {
     name: "Merel Theisen",
     slug: "merel-theisen",
-    image: "./src/assets/authors/about-me.png",
+    image: aboutMe,
     bio: "",
   }
 ];
